125b8334a6f430ec5b4df2001cf02f120de7bd7e
Devin Silvia <devin.silvia@gmail.com>
2012-07-09 13:30:15 -0600

    removed ".down_species" from the "species_reactions" function in chemical_network.py.  It became apparent when running "write_species_deriv" that the species creation terms were missing from the _p equations.  For the list of species reactions, we do, in fact, want every reaction a given species is involved in order to make sure we track when species are both created and destroyed.

--- test_oxy_rates.py
+++ test_oxy_rates.py
@@ -2,6 +2,7 @@ from dengo.chemical_network import ChemicalNetwork
 from dengo.reaction_classes import Species, chianti_rate, reaction_registry
 import dengo.primordial_rates
 import dengo.oxygen_rates
+from dengo.solver_writer import write_reaction, write_species_deriv
 
 oxygen_network = ChemicalNetwork()
 
@@ -16,3 +17,10 @@ for species in oxygen_network.required_species:
     print "Querying: ", species
     for rxn in oxygen_network.species_reactions(species):
         print "  ", rxn
+
+for rxn in oxygen_network:
+    write_reaction(rxn)
+
+for species in oxygen_network.required_species:
+    print species.name
+    write_species_deriv(species, oxygen_network.species_reactions(species))
