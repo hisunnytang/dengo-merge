f664d312044f14d1e53ed25ec92e7ea110a75cfa
Devin Silvia <devin.silvia@gmail.com>
2012-07-10 13:38:59 -0600

    added HDF5 file write to test_oxy_rates.py -- currently stores the ionization/recombination rates

--- test_oxy_rates.py
+++ test_oxy_rates.py
@@ -1,5 +1,7 @@
+import numpy as na
+import h5py
 from dengo.chemical_network import ChemicalNetwork
-from dengo.reaction_classes import Species, chianti_rate, reaction_registry
+from dengo.reaction_classes import Species, chianti_rate, reaction_registry, species_registry
 import dengo.primordial_rates
 import dengo.oxygen_rates
 from dengo.solver_writer import write_reaction, write_species_deriv
@@ -24,3 +26,17 @@ for rxn in oxygen_network:
 for species in oxygen_network.required_species:
     print species.name
     write_species_deriv(species, oxygen_network.species_reactions(species))
+
+# Define the temperature we want rate values for
+Tlog_bounds = (1, 9) 
+T = na.logspace(Tlog_bounds[0], Tlog_bounds[1], 1024)
+
+# Write the rate values to file
+f = h5py.File('oxygen_rates_table.h5', 'w')
+for rxn_name in na.sort(oxygen_network.reactions.keys()):
+    print 'Reaction %s is being written to HDF5 File...' %rxn_name
+    dset = f.create_dataset('/%s' %rxn_name,
+                            data=oxygen_network.reactions[rxn_name].coeff_fn(T).astype('float64'))
+    # Store the reaction equation as an attribute of the dataset
+    dset.attrs.create('reaction_equation', data=oxygen_network.reactions[rxn_name].__str__())
+f.close()
