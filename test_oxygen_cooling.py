54f32a8846055bfcb7e509dfc96cbd8b08601f18
Devin Silvia <devin.silvia@gmail.com>
2012-07-24 16:46:33 -0600

    modifications to test_oxygen_cooling.py -- now outputs cooling curve plot

--- test_oxygen_cooling.py
+++ test_oxygen_cooling.py
@@ -5,6 +5,7 @@ from dengo.chemical_network import \
 import dengo.primordial_rates, dengo.primordial_cooling
 import dengo.oxygen_rates, dengo.oxygen_cooling
 from dengo.solver_writer import write_reaction, write_species_deriv
+from matplotlib import pylab
 
 oxygen = ChemicalNetwork()
 for ca in cooling_registry.values():
@@ -21,8 +22,13 @@ for s in reaction_registry.values():
 print "These species are required for chemistry and cooling:"
 print "\n".join([s.name for s in sorted(oxygen.required_species)])
 
-oxygen.init_temperature()
+oxygen.init_temperature(T_bounds=(1.0e4, 1.0e8))
 
-o_6_c = cooling_registry['o_6_c']
-o_6_c.tables['o_6_c'](oxygen)
+# Let's create a cooling function plot for all oxygen ion species
+pylab.ion()
+pylab.figure(1)
+for cooling_action in oxygen.cooling_actions.values():
+    pylab.loglog(oxygen.T, cooling_action.tables[cooling_action.name](oxygen), label=cooling_action.name)
+
+pylab.legend()
 
