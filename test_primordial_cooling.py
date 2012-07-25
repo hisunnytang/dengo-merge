adf53dd5492dc34fcae45b3483c89eaa9e5d064e
Matthew Turk <matthewturk@gmail.com>
2012-07-25 15:36:21 -0400

    Adding a sympy printer

--- test_primordial_cooling.py
+++ test_primordial_cooling.py
@@ -4,6 +4,7 @@ from dengo.chemical_network import \
     cooling_registry
 import dengo.primordial_rates, dengo.primordial_cooling
 from dengo.solver_writer import write_reaction, write_species_deriv
+from sympy.printing import print_ccode
 
 primordial = ChemicalNetwork()
 for ca in cooling_registry.values():
@@ -18,3 +19,6 @@ for s in reaction_registry.values():
 print "These species are required for chemistry and cooling:"
 print "\n".join([s.name for s in sorted(primordial.required_species)])
 
+for species in primordial.required_species:
+    eq = primordial.species_total(species)
+    print_ccode(eq, assign_to = "d_%s" % species.name)
