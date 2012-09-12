cdcd05d089fd354a6a0e278f68bb33a8861649e9
Devin Silvia <devin.silvia@gmail.com>
2012-09-12 17:40:26 -0600

    added a write_oxygen_network.py to test the solver with oxygen.  It seems to be suffering for the same issues as the primordial network.  rhs_total is always equal to rhs[de].  Things explode into unrealistic values.  This may still have something to do with how electrons are being handled.

---
+++ examples/write_oxygen_network.py
@@ -0,0 +1,68 @@
+from dengo.chemical_network import \
+    ChemicalNetwork, \
+    reaction_registry, \
+    cooling_registry
+import dengo.primordial_rates, dengo.primordial_cooling
+import dengo.oxygen_rates, dengo.oxygen_cooling
+from dengo.write_rate_reader import \
+    create_rate_tables, \
+    create_rate_reader, \
+    create_initial_conditions
+from dengo.chemistry_constants import tiny, kboltz, mh
+
+oxygen = ChemicalNetwork()
+for ca in cooling_registry.values():
+    if ca.name.startswith("o_"):
+        oxygen.add_cooling(ca)
+
+for s in reaction_registry.values():
+    if s.name.startswith("o_"):
+        oxygen.add_reaction(s)
+
+oxygen.init_temperature((1e4, 1e8))
+
+create_rate_tables(oxygen, "oxygen")
+create_rate_reader(oxygen, "oxygen")
+
+# Generate initial conditions (switch to False to disable this)
+generate_initial_conditions = True
+
+if generate_initial_conditions:
+    import numpy as na
+    NCELLS = 64
+    density = 1.0e3
+    init_array = na.ones(NCELLS) 
+    X = 1.0e-4
+
+    init_values = dict()
+    init_values['density'] = density * init_array
+    init_values['o_1'] = init_array # use conservation to set this below
+    # populate initial fractional values for the other species
+    for s in sorted(oxygen.required_species):
+        if s.name != 'ge' and s.name != 'o_1':
+            if s.name == 'de' or s.name == 'o_2':
+                init_values[s.name] = X * init_array
+            else:
+                init_values[s.name] = tiny * init_array
+            init_values['o_1'] -= init_values[s.name]
+
+    # convert to masses to multiplying by the density factor
+    for iv in init_values:
+        init_values[iv] *= density
+    
+    # set up initial temperatures values used to define ge
+    temperature = na.logspace(4, 8, NCELLS)
+
+    # calculate the number density
+    number_density = 0.0
+    for s in sorted(oxygen.required_species):
+        if s.name != 'ge':
+            number_density += init_values[s.name]/s.weight
+            init_values[s.name] /= s.weight
+
+    # calculate ge (very crudely)
+    gamma = 5.e0/3.e0
+    init_values['ge'] = ((temperature * density * kboltz)
+                         / (number_density * mh * (gamma - 1)))
+    
+    create_initial_conditions(init_values, 'oxygen')
