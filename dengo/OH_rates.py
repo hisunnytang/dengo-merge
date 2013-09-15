e66fd427ff22f83f80d2c3360276429f86ac58d2
Asna Ansari <asna.ansari@gmail.com>
2013-09-15 13:03:39 -0400

    rate-retrieving modules for H2O, O2, H2, OH, and CO formation reactions now work

--- dengo/OH_rates.py
+++ dengo/OH_rates.py
@@ -1,11 +1,13 @@
 # Gets rates for all OH formation reactions in the UMIST database
 
-from reaction_class import Species, species_registry
-import umist_rates
+from reaction_classes import Species, species_registry
+from umist_rates import umist_rates
+
 
 atomicSymbol = 'OH'
 atomicNumber = -1
 atomicWeight = 17
+i = -1
 
 # make species s with these params
 
@@ -13,10 +15,10 @@ speciesName = "%s" %(atomicSymbol)
     # Check if the species already exists
     # in the species registry, if it does
     # we don't want to create it again
-    if (speciesName in species_registry) == False:
-        s = Species(speciesName, atomicNumber, atomicWeight, i)
-    else:
-        s = species_registry[speciesName]
+if (speciesName in species_registry) == False:
+    s = Species(speciesName, atomicNumber, atomicWeight, i)
+else:
+    s = species_registry[speciesName]
 
 
 umist_rates(s)
--- dengo/Rate.py
+++ dengo/Rate.py
@@ -12,49 +12,54 @@ def get_rate(reaction, temp):
     a = float(reaction[9])
     b = float(reaction[10])
     g = float(reaction[11])
-    T_lower = float(reaction[12])
-    T_upper = float(reaction[13])
+    T_lower = int(reaction[12])
+    T_upper = int(reaction[13])
     
-    if (reactantA in species_registry) == False:
-        rA = Species(reactantA, -1, -1, -1)
-    else:
-        rA = species_registry[reactantA]
-    if (reactantB in species_registry) == False:
-        rB = Species(reactantB, -1, -1, -1)
-    else:
-        rB = species_registry[reactantB]
-    if (prodC in species_registry) == False:
-        pC = Species(prodC, -1, -1, -1)
-    else:
-        pC = species_registry[prodC]
+    if (temp >= T_lower and temp <= T_upper):
+        if (reactantA in species_registry) == False:
+            rA = Species(reactantA, -1, -1, -1)
+        else:
+            rA = species_registry[reactantA]
+        if (reactantB in species_registry) == False:
+            rB = Species(reactantB, -1, -1, -1)
+        else:
+            rB = species_registry[reactantB]
+        if (prodC in species_registry) == False:
+            pC = Species(prodC, -1, -1, -1)
+        else:
+            pC = species_registry[prodC]
     
-    if (prodD in species_registry) == False:
-        pD = Species(prodD, -1, -1, -1)
-    else:
-        pD = species_registry[prodD]
+        if (prodD in species_registry) == False:
+            pD = Species(prodD, -1, -1, -1)
+        else:
+            pD = species_registry[prodD]
     
-    if (prodE in species_registry) == False:
-        pE = Species(prodE, -1, -1, -1)
-    else:
-        pE = species_registry[prodE]
+        if (prodE in species_registry) == False:
+            pE = Species(prodE, -1, -1, -1)
+        else:
+            pE = species_registry[prodE]
    
-    if (prodF in species_registry) == False:
-        pF = Species(prodF, -1, -1, -1)
+        if (prodF in species_registry) == False:
+            pF = Species(prodF, -1, -1, -1)
+        else:
+            pF = species_registry[prodF]
+        
+        
+        if type == 'CP':
+            rate = a # rate coefficient with units 1/s
+            units = "1/s"
+            return rate, units
+        elif type == 'CR':
+            rate = 0.0
+            units = ''
+            return rate, units
+        else:
+            t = float(temp) / float(300)
+            rate = a*(math.pow(t,b))*(math.exp(-g / temp)) # rate coefficient with units cm^3 / s
+            units = "cm^3 / s"
+            return rate, units
+        return Reaction("%s+%s" % (rA.name, rB.name), rate, [(1, rA.name), (1, rB.name)], [(1, pC.name), (1, pD.name), (1, pE.name), (1,pF.name)]) 
     else:
-        pF = species_registry[prodF]
-        
-        
-    if type == 'CP':
-        rate = a # rate coefficient with units 1/s
-        units = "1/s"
-        return rate, units
-    elif type == 'CR':
-        rate = 0.0
+        rate = 0
         units = ''
         return rate, units
-    else:
-        t = float(temp) / float(300)
-        rate = a*(math.pow(t,b))*(math.exp(-g / temp)) # rate coefficient with units cm^3 / s
-        units = "cm^3 / s"
-    return rate, units
-    return Reaction("%s+%s" % (rA.name, rB.name), rate, [(1, rA.name), (1, rB.name)], [(1, pC.name), (1, pD.name), (1, pE.name), (1,pF.name)]) 
