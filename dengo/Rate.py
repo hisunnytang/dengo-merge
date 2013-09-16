6d750d04693c2a8256fdbad475a666a9001d6d3d
Asna Ansari <asna.ansari@gmail.com>
2013-09-16 12:36:58 -0400

    consolidated **_rates.py files into a single get_rates function & removed stray write_oxygen_network file from dengo subdir

--- dengo/Rate.py
+++ dengo/Rate.py
@@ -1,14 +1,22 @@
 import math
 from reaction_classes import Species, species_registry, Reaction
 
+#Check whether the species exists and if not, add to species registry
+def _ensure_species(sp):
+    if sp not in species_registry:
+        sp = Species(sp,-1,-1,-1)
+    else:
+        sp = speces_registry[sp]
+    return sp
+
 def get_rate(reaction, temp):
     type = reaction[1]
-    reactantA = reaction[2]
-    reactantB = reaction[3]
-    prodC = reaction[4]
-    prodD = reaction[5]
-    prodE = reaction[6]
-    prodF = reaction[7]
+    reactantA = _ensure_species(reaction[2])
+    reactantB = _ensure_species(reaction[3])
+    prodC = _ensure_species(reaction[4])
+    prodD = _ensure_species(reaction[5])
+    prodE = _ensure_species(reaction[6])
+    prodF = _ensure_species(reaction[7])
     a = float(reaction[9])
     b = float(reaction[10])
     g = float(reaction[11])
@@ -16,34 +24,6 @@ def get_rate(reaction, temp):
     T_upper = int(reaction[13])
     
     if (temp >= T_lower and temp <= T_upper):
-        if (reactantA in species_registry) == False:
-            rA = Species(reactantA, -1, -1, -1)
-        else:
-            rA = species_registry[reactantA]
-        if (reactantB in species_registry) == False:
-            rB = Species(reactantB, -1, -1, -1)
-        else:
-            rB = species_registry[reactantB]
-        if (prodC in species_registry) == False:
-            pC = Species(prodC, -1, -1, -1)
-        else:
-            pC = species_registry[prodC]
-    
-        if (prodD in species_registry) == False:
-            pD = Species(prodD, -1, -1, -1)
-        else:
-            pD = species_registry[prodD]
-    
-        if (prodE in species_registry) == False:
-            pE = Species(prodE, -1, -1, -1)
-        else:
-            pE = species_registry[prodE]
-   
-        if (prodF in species_registry) == False:
-            pF = Species(prodF, -1, -1, -1)
-        else:
-            pF = species_registry[prodF]
-        
         
         if type == 'CP':
             rate = a # rate coefficient with units 1/s
