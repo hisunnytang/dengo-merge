006bc7598bd5d59ac75af607de12a9ab19d03f6f
Matthew Turk <matthewturk@gmail.com>
2013-09-23 11:24:12 -0400

    A few minor fixes for UMIST

--- dengo/get_rates.py
+++ dengo/get_rates.py
@@ -7,18 +7,18 @@ def get_rates(atomicSymbol, atomicWeight, atomicNumber):
     speciesName = "%s" %(atomicSymbol)
 
     if speciesName not in species_registry:
-        s = Species(speciesName, atomicNumber, atomicWeight, i)
+        s = Species(speciesName, atomicNumber, atomicWeight, 0)
     else:
         s = species_registry[speciesName]
 
     umist_rates(s)
     
 
-get_rates(CO, 28, -1)
-get_rates(C, 12, -1)
-get_rates(O, 16, -1)
-get_rates(OH, 17, -1)
-get_rates(H, 1, -1)
-get_rates(H2, 2, -2)
-get_rates(H2O, 18, -1)
-get_rates(O2, 32, -1)
+get_rates("CO", 28, -1)
+get_rates("C", 12, -1)
+get_rates("O", 16, -1)
+get_rates("OH", 17, -1)
+get_rates("H", 1, -1)
+get_rates("H2", 2, -2)
+get_rates("H2O", 18, -1)
+get_rates("O2", 32, -1)
--- dengo/umist_rates.py
+++ dengo/umist_rates.py
@@ -11,13 +11,8 @@ def umist_rates(species):
 		for i,line in enumerate(lines):  
 			reaction = re.split(":?", line)
 			if (reaction[2]==r1 or reaction[3]==r1 or reaction[4]==r1 or reaction[5]==r1): 
-				print "%s + %s -> %s + %s + %s + %s. The reaction type is %s" % (reaction[2], reaction[3], reaction[4], reaction[5], reaction[6], reaction[7], reaction[1])
-				#temp = network.T
-				#Use a dummy temp variable for tests
 				temp = 500
-				rate = Rate.get_rate(reaction, temp)[0]
-				units = Rate.get_rate(reaction, temp)[1]
-				print "The reaction rate at %s K is %s %s." %(temp, rate, units)
+				Rate.get_rate(reaction, temp)
 		
 
 
