e805333a8b5416bec3a0ccc3f40499ed274da195
Asna Ansari <asna@bitbucket.org>
2013-09-23 21:16:02 -0400

    added UMIST reaction network

--- dengo/get_rates.py
+++ dengo/get_rates.py
@@ -5,20 +5,19 @@ from umist_rates import umist_rates
 
 def get_rates(atomicSymbol, atomicWeight, atomicNumber):
     speciesName = "%s" %(atomicSymbol)
-
+    free_electrons = speciesName.count('+')-speciesName.count('-')
     if speciesName not in species_registry:
-        s = Species(speciesName, atomicNumber, atomicWeight, 0)
+        s = Species(speciesName, atomicNumber, atomicWeight, free_electrons)
     else:
         s = species_registry[speciesName]
 
     umist_rates(s)
-    
 
-get_rates("CO", 28, -1)
-get_rates("C", 12, -1)
-get_rates("O", 16, -1)
-get_rates("OH", 17, -1)
-get_rates("H", 1, -1)
-get_rates("H2", 2, -2)
-get_rates("H2O", 18, -1)
-get_rates("O2", 32, -1)
+get_rates('CO', 28, -1)
+get_rates('C', 12, -1)
+get_rates('O', 16, -1)
+get_rates('OH', 17, -1)
+get_rates('H', 1, -1)
+get_rates('H2', 2, -2)
+get_rates('H2O', 18, -1)
+get_rates('O2', 32, -1)
--- dengo/umist_rates.py
+++ dengo/umist_rates.py
@@ -3,7 +3,7 @@
 import re
 import Rate
 
-def umist_rates(species):
+def umist_rates(species, network):
 	r1 = species.name
 	with open("RATE12.txt", "r") as f:
 		lines = f.readlines()
@@ -11,7 +11,7 @@ def umist_rates(species):
 		for i,line in enumerate(lines):  
 			reaction = re.split(":?", line)
 			if (reaction[2]==r1 or reaction[3]==r1 or reaction[4]==r1 or reaction[5]==r1): 
-				temp = 500
+				temp = network.T
 				Rate.get_rate(reaction, temp)
 		
 
