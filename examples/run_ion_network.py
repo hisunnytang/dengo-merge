fc6878ea18c15d989c157aab7ac8ea5c2287eeae
Devin Silvia <devin.silvia@gmail.com>
2014-03-12 09:12:22 -0400

    Adding a run_ion_network example.
    Couple of other minor changes as well, including a change to tolerance.

--- examples/run_ion_network.py
+++ examples/run_ion_network.py
@@ -1,42 +1,30 @@
+import numpy as np
 from dengo.chemical_network import \
     ChemicalNetwork, \
     reaction_registry, \
     cooling_registry
-from dengo.primordial_rates import setup_primordial
-import dengo.primordial_cooling
 from dengo.ion_by_ion import setup_ionization
 from dengo.chemistry_constants import tiny, kboltz, mh
 import numpy as np
 
-NCELLS = 1
-density = 1e-3 * 1.67e-24
-temperature = np.logspace(4, 6.7, NCELLS)
-temperature[:] = 5e6
-X = 1e-3
+NCELLS = 32
+density = 1.0 * 1.67e-24
+temperature = np.logspace(2, 8, NCELLS)
+X = 1e-8
 
 ion_by_ion = ChemicalNetwork(write_intermediate = False,
                              stop_time = 3.1557e13)
 ion_by_ion.add_species("de")
 
-#for atom in ["O", "C", "Si", "Mg", "N", "S", "He", "Ne", "H"]:
-for atom in ["H", "O", "He"]:
+for atom in ["H", "He", "C", "N", "O", "Ne", "Mg", "Si", "S"]:
     s, c, r = setup_ionization(atom)
     ion_by_ion.add_collection(s, c, r)
-    #ion_by_ion.add_collection(s, [], r)
-
-ion_by_ion.add_cooling('compton')
-
-#s, c, r = setup_primordial()
-#ion_by_ion.add_collection(s, c, r)
 
 # This defines the temperature range for the rate tables
 ion_by_ion.init_temperature((1e0, 1e12))
 
 # This defines the redsfhit range for the rate tables
 ion_by_ion.init_redshift((0.0, 9.0))
-
-# Want intermediate output?
-#combined.write_intermediate_solutions = True
 
 tiny = 1e-10
 
@@ -78,8 +66,6 @@ else:
                 init_values[s.name][init_values[s.name] < tiny] = tiny
 
     init_values['de']      = init_array * 0.0
-#    total_density = combined.calculate_total_density(init_values, ("OI",))
-#    init_values["OI"] = init_array.copy() - total_density
     init_values = ion_by_ion.convert_to_mass_density(init_values)
 
 init_values['de'] = ion_by_ion.calculate_free_electrons(init_values)
@@ -91,8 +77,35 @@ gamma = 5.0/3.0
 init_values['ge'] = ((temperature * number_density * kboltz)
                      / (init_values['density'] * mh * (gamma - 1)))
 
-
-    # Write the initial conditions file
+# Write the initial conditions file
 ion_by_ion.write_solver("ion_by_ion", output_dir = ".",
                         init_values=init_values,
                         input_is_number=False)
+
+import pyximport
+pyximport.install(setup_args={"include_dirs":np.get_include()},
+                  reload_support=True, inplace=True)
+
+ion_by_ion_solver_run = pyximport.load_module("ion_by_ion_solver_run",
+                            "ion_by_ion_solver_run.pyx",
+                            build_inplace = True, pyxbuild_dir = "_dengo_temp")
+rv, rv_int = ion_by_ion_solver_run.run_ion_by_ion(init_values, 1e16)
+import pylab
+pylab.clf()
+
+mask = rv_int['successful']
+for name in sorted(rv_int):
+    if len(rv_int[name].shape) == 1:
+        rv_int[name] = rv_int[name][mask]
+    else:
+        rv_int[name] = rv_int[name][0, mask]
+    
+skip = ('successful', 'dt', 't', 'ge')
+for n, v in sorted(rv_int.items()):
+    if n in skip: continue
+    pylab.loglog(rv_int['t'], v, label = n)
+
+pylab.ylim(density * 1e-20, density * 10)
+pylab.xlabel("time [s]")
+pylab.legend(loc='best', fontsize='xx-small')
+pylab.savefig("plot.png")
