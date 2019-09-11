79eaf6c2d74824cb572f712d3d902dbecf22b46e
Sunny Tang <hisunnytang@gmail.com>
2019-09-11 13:56:32 -0500

    added required dependencies to install KLU

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -1,3 +1,7 @@
 set -ex
 git clone https://github.com/jluttine/suitesparse.git
-cd suitesparse/KLU && make library
+cd suitesparse/SuiteSparse_config && make && cd ../
+cd suitesparse/AMD && make && cd ../
+cd suitesparse/COLAMD && make && cd ../
+cd suitesparse/BTF && make && cd ../
+cd suitesparse/KLU && make
