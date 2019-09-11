61c032559d861a112b9489a28f8391880a6b892b
Sunny Tang <hisunnytang@gmail.com>
2019-09-11 13:31:08 -0500

    update the path in installation shell script

--- install-chiantipy.sh
+++ install-chiantipy.sh
@@ -1,5 +1,5 @@
 wget http://www.chiantidatabase.org/download/CHIANTI_9.0.1_data.tar.gz
 tar -xvzf CHIANTI_9.0.1_data.tar.gz
 pip install ChiantiPy
-export XUVTOP="~/CHIANTI_data"
+export XUVTOP=~/virtualenv/CHIANTI_data
 
--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -1,3 +1,3 @@
 set -ex
 git clone https://github.com/jluttine/suitesparse.git
-cd ~/suitesparse/KLU && make library
+cd ~/virtualenv/suitesparse/KLU && make library
