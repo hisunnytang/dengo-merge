c71ef240a8b0420cd8b2c5b45c83e1b2d1d4f345
Sunny Tang <hisunnytang@gmail.com>
2019-09-15 22:50:46 -0500

    make entire suitesparse library

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -1,6 +1,4 @@
 set -ex
 git clone https://github.com/jluttine/suitesparse.git
 cd suitesparse/SuiteSparse_config && make && cd ../
-cd AMD && make && cd ../
-cd BTF && make && cd ../
-cd KLU && make
+make
