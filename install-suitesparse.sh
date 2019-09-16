20b84bee631a23f27352148500461dc5fe080b24
Sunny Tang <hisunnytang@gmail.com>
2019-09-16 10:44:35 -0500

    install gfortran, so that openblas can install its optimized lapack

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -4,9 +4,11 @@ cd suitesparse
 git clone https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS && make && make install PREFIX="/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install"
 cd ../
-export BLAS="-L/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install -lopenblas"
+export BLAS="-L/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install/lib -lopenblas -llapack"
 cd SuiteSparse_config && make && cd ../
 cd AMD && make && cd ../
+cd CAMD && make && cd ../
+cd metis-5.1.0 && make && cd ../
 cd COLAMD && make && cd ../
 cd BTF && make && cd ../
 cd KLU && make
