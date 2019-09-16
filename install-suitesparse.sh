f32ecd7dba55163bfa96e73f8c0f9ec60b21db40
Sunny Tang <hisunnytang@gmail.com>
2019-09-16 10:15:56 -0500

    install openblas before suitesparse

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -1,4 +1,12 @@
 set -ex
 git clone https://github.com/jluttine/suitesparse.git
+cd suitesparse
+git clone https://github.com/xianyi/OpenBLAS.git
+cd OpenBLAS && make && make install PREFIX="/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install"
+cd ../
+export BLAS="-L/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install -lopenblas"
 cd suitesparse/SuiteSparse_config && make && cd ../
-make
+cd AMD && make && cd ../
+cd COLAMD && make && cd ../
+cd BTF && make && cd ../
+cd KLU && make
