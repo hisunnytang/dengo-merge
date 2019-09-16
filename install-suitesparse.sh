c797e4ccf8cbcf3133e5ce8d4800ade52d790673
Sunny Tang <hisunnytang@gmail.com>
2019-09-15 22:46:17 -0500

    update travis.yml and remove installation for CHOLMOD

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -2,6 +2,5 @@ set -ex
 git clone https://github.com/jluttine/suitesparse.git
 cd suitesparse/SuiteSparse_config && make && cd ../
 cd AMD && make && cd ../
-cd COLAMD && make && cd ../
 cd BTF && make && cd ../
 cd KLU && make
