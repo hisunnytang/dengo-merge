54ffcb806e931af9116ca6eebbff886907a7059e
Sunny Tang <hisunnytang@gmail.com>
2019-09-16 10:28:06 -0500

    fix the directory set in suitesparse installation script

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -5,7 +5,7 @@ git clone https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS && make && make install PREFIX="/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install"
 cd ../
 export BLAS="-L/home/travis/build/hisunnytang/dengo-merge/suitesparse/OpenBLAS/Install -lopenblas"
-cd suitesparse/SuiteSparse_config && make && cd ../
+cd SuiteSparse_config && make && cd ../
 cd AMD && make && cd ../
 cd COLAMD && make && cd ../
 cd BTF && make && cd ../
