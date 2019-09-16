16dcb0a96338949b851de348bae713cfef6ad107
Sunny Tang <hisunnytang@gmail.com>
2019-09-15 22:39:13 -0500

    trying to install packages on travis

---
+++ install-grackle.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/grackle-project/grackle.git
+cd grackle && ./configure && cd src/clib
+make show-config
+make
+make install
--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -1,7 +1,7 @@
 set -ex
 git clone https://github.com/jluttine/suitesparse.git
 cd suitesparse/SuiteSparse_config && make && cd ../
-cd suitesparse/AMD && make && cd ../
-cd suitesparse/COLAMD && make && cd ../
-cd suitesparse/BTF && make && cd ../
-cd suitesparse/KLU && make
+cd AMD && make && cd ../
+cd COLAMD && make && cd ../
+cd BTF && make && cd ../
+cd KLU && make
