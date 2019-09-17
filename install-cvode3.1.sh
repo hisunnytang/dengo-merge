60578f0002fe0c6916378bb900acecb62aadddd4
Sunny Tang <hisunnytang@gmail.com>
2019-09-16 22:39:50 -0500

    remove the original builddir for my cvode3.1 copy

--- install-cvode3.1.sh
+++ install-cvode3.1.sh
@@ -1,6 +1,7 @@
 set -ex
 tar -xvzf cvode-3.1.0.tar.gz
-cd cvode-3.1.0/builddir
+cd cvode-3.1.0 && mkdir builddir
+cd builddir
 cvode_inst="${TRAVIS_BUILD_DIR}/cvode_instdir"
 suitesparse_inst="${TRAVIS_BUILD_DIR}/suitesparse"
 cmake -DCMAKE_INSTALL_PREFIX=$cvode_inst -DKLU_INCLUDE_DIR="${suitesparse_inst}/include" -DKLU_LIBRARY_DIR="${suitesparse}/KLU/lib" -DCMAKE_C_FLAGS=-fPIC -DKLU_ENABLE=ON ../
