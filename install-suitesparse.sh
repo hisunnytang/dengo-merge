5015742ee49834e652b88fe123eb68b1966213f2
root <hisunnytang@gmail.com>
2021-04-07 23:05:52 +0000

    use lapack/ blas in suitesparse

--- install-suitesparse.sh
+++ install-suitesparse.sh
@@ -2,14 +2,13 @@ git clone https://github.com/DrTimothyAldenDavis/SuiteSparse
 cd SuiteSparse
 make purge
 # make distclean
-unset LAPACK
-unset BLAS
-cd SuiteSparse_config && make CUDA=no && cd ../
-make metis CUDA=no
-cd AMD && make CUDA=no && cd ../
-cd CAMD && make CUDA=no && cd ../
-cd CCOLAMD && make CUDA=no && cd ../
-cd COLAMD && make CUDA=no && cd ../
-cd BTF && make CUDA=no && cd ../
+export CUDA=no
+cd SuiteSparse_config && make CUDA=no &> /dev/null && cd ../
+make metis CUDA=no &> /dev/null
+cd AMD && make CUDA=no &> /dev/null && cd ../
+cd CAMD && make CUDA=no &> /dev/null && cd ../
+cd CCOLAMD && make CUDA=no &> /dev/null && cd ../
+cd COLAMD && make CUDA=no &> /dev/null && cd ../
+cd BTF && make CUDA=no &> /dev/null && cd ../
 cd CHOLMOD && make CUDA=no && cd ../
 cd KLU && make CUDA=no
