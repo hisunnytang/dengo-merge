ab7140bdd7376bce96a7f2a776c8fcc703cc04f4
Sunny@dxl2 <kwoksun2@dxl2.ncsa.illinois.edu>
2021-06-03 16:03:10 -0500

    remove redundant paths

--- examples/test/test_freefall.py
+++ examples/test/test_freefall.py
@@ -1,28 +1,9 @@
 from evolve_free_fall import run_dengo_freefall, compare_dengo_grackle
-from utilities import set_env_variables
+from utilities import check_defined_envpath
 import pytest
 import os
 
-#print("---------------------------")
-#print(os.environ)
-#print(os.environ["TRAVIS_BUILD_DIR"])
-#print("--------------------------")
-
-if "TRAVIS_BUILD_DIR" not in os.environ:
-    set_env_variables("HDF5_DIR", "/home/kwoksun2/anaconda3")
-    set_env_variables("CVODE_PATH", "/home/kwoksun2/cvode-3.1.0/instdir")
-    set_env_variables("HDF5_PATH", "/home/kwoksun2/anaconda3")
-    set_env_variables("SUITESPARSE_PATH", "/home/kwoksun2/SuiteSparse")
-    set_env_variables("DENGO_INSTALL_PATH", "/home/kwoksun2/dengo_install")
-else:
-    # then we assume that the libraries are installed relative to the dengo
-    # path, so the below paths are the relative default install path
-    set_env_variables("HDF5_DIR", "hdf5_install")
-    set_env_variables("CVODE_PATH", "cvode-3.1.0/instdir")
-    set_env_variables("HDF5_PATH", "hdf5_install")
-    set_env_variables("SUITESPARSE_PATH", "suitesparse")
-    set_env_variables("DENGO_INSTALL_PATH", "dengo_install")
-
+check_defined_envpath()
 
 @pytest.fixture
 def setup_solver_options(update_options={}):
@@ -39,18 +20,6 @@ def setup_solver_options(update_options={}):
 
 
 def test_freefall(setup_solver_options):
-
-    if "TRAVIS_BUILD_DIR" not in os.environ:
-        os.environ["DENGO_PATH"] = "/home/kwoksun2/dengo-git"
-
-    # if you install it through the install scripts
-    set_env_variables("HDF5_DIR", "hdf5_install")
-    set_env_variables("CVODE_PATH", "cvode-3.1.0/instdir")
-    set_env_variables("HDF5_PATH", "hdf5_install")
-    set_env_variables("SUITESPARSE_PATH", "suitesparse")
-    set_env_variables("DENGO_INSTALL_PATH", "dengo_install")
-
-
     run_dengo_freefall(setup_solver_options)
     compare_dengo_grackle(setup_solver_options["output_dir"])
 
