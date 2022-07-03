617bccb0fde71ec327c9ba24ab213286a89edb13
Sunny@dxl2 <kwoksun2@dxl2.ncsa.illinois.edu>
2022-07-03 00:22:09 -0500

    remove redundant test files

--- examples/test/temp/predator_prey_solver_main.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import pyximport
-pyximport.install(setup_args={"include_dirs":numpy.get_include()},
-                  reload_support=True)
-
-import predator_prey_solver_run
-predator_prey_solver_run.main_run_predator_prey()
